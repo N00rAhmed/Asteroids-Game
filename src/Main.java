ce056aabfd6cf7068f63ed9b7fe0a457bd76e08e
N00rAhmed <tronn232003@gmail.com>
2024-03-28 08:29:59 +0000

    created a ui frame

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
-
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		JFrame frame = new JFrame(); // creates a frame
+		frame.setSize(420,420); //set x and y dimension on frame
+		frame.setVisible(true); //make frame visible
+
 		System.out.println("Welcome to the Java Asteroids Game");
-		System.out.println("Made by Noor Ahmed");
 
 	}
 
