34cd37b2152011db15efee9de47ba6b2a78fdd36
N00rAhmed <tronn232003@gmail.com>
2024-03-28 05:03:25 +0000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the Java Asteroids Game");
+		System.out.println("Made by Noor Ahmed");
+
+	}
+
+}
