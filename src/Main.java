862d9dcece07904f14ae65ddf497ab9f46bc7cf4
N00rAhmed <tronn232003@gmail.com>
2024-03-30 13:22:48 +0000

    added classes and methods

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,20 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		JFrame frame = new JFrame(); // creates a frame
-		frame.setTitle("Asteroids Game"); // sets title of frame
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false); // stops frame being resized
-		frame.setSize(850,600); //set x and y dimension on frame
-		frame.setVisible(true); //make frame visible
-
-		System.out.println("Welcome to the Java Asteroids Game");
-
-		ImageIcon image = new ImageIcon("logo.jpg"); // create image icon
-		frame.setIconImage(image.getImage()); // change icon of frame
+		MyFrame myFrame = new MyFrame();
 		
-		frame.getContentPane().setBackground(Color.black);
+//		JFrame frame = new JFrame(); // creates a frame
+//		frame.setTitle("Asteroids Game"); // sets title of frame
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		frame.setResizable(false); // stops frame being resized
+//		frame.setSize(850,600); //set x and y dimension on frame
+//		frame.setVisible(true); //make frame visible
+//
+//		System.out.println("Welcome to the Java Asteroids Game");
+//
+//		ImageIcon image = new ImageIcon("logo.jpg"); // create image icon
+//		frame.setIconImage(image.getImage()); // change icon of frame
+//		
 	}
 
 }
---
+++ src/MyFrame.java
@@ -0,0 +1,82 @@
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class MyFrame extends JFrame {
+
+	Image image;
+	Graphics graphics;
+	Spaceship player;
+	Spaceship enemy;
+	boolean gameOver;
+	
+	
+	MyFrame(){
+
+		player = new Spaceship(100,300,50,50,Color.blue);
+		enemy = new Spaceship(400,300,50,50,Color.red);
+		gameOver = false;
+
+//		JFrame frame = new JFrame(); // creates a frame
+		this.setTitle("Asteroids Game"); // sets title of frame
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false); // stops frame being resized
+		this.setSize(850,600); //set x and y dimension on frame
+
+		
+		this.setVisible(true); //make frame visible
+		this.addKeyListener(new AL());
+
+		System.out.println("Welcome to the Java Asteroids Game");
+
+		ImageIcon image = new ImageIcon("logo.jpg"); // create image icon
+		this.setIconImage(image.getImage()); // change icon of frame
+		
+		
+//		this.getContentPane().setBackground(Color.black);
+		
+	}
+	
+	public void paint(Graphics g){
+		
+	    // Set the background colour
+		g.setColor(Color.black);
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+	    
+		// Create an image and get its graphics for double buffering
+		image = createImage(this.getWidth(),this.getHeight());
+		graphics = image.getGraphics();
+	    
+		// Draw the background color on the image's graphics
+		graphics.setColor(Color.black);
+		graphics.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+		// Draw the spaceships on the image's graphics
+		player.draw(graphics);
+		enemy.draw(graphics);
+
+	    // Draw the image on the frame's graphics
+		g.drawImage(image,0,0,this);
+
+	}
+	
+
+	
+	
+	
+	
+	public void checkCollision() {
+		
+	}
+	public class AL extends KeyAdapter{
+		@Override
+		public void keyPressed(KeyEvent e) {
+			
+			player.keyPressed(e);
+			checkCollision();
+			repaint();
+			
+		}
+	}
+}
