5003f0ba354a3b3eb2dd7550aa4c42527e61a941
N00rAhmed <tronn232003@gmail.com>
2024-04-27 20:43:12 +0100

    created an asteroids class

--- src/Asteroids.java
+++ src/Asteroids.java
@@ -1,4 +1,21 @@
+import java.awt.Rectangle;
+import java.awt.Color;
 
-public class Asteroids {
-
+public class Asteroids extends Rectangle {
+	
+	Color color;
+	
+	Asteroids(int x, int y, int width, int height, Color color){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.color = color;
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	
 }
