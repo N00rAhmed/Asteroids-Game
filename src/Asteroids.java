1e72e050b4f40716e406cc5d73ac3d50d3b1d51e
N00rAhmed <tronn232003@gmail.com>
2024-05-04 11:34:31 +0100

    removed comments

--- src/Asteroids.java
+++ src/Asteroids.java
@@ -18,25 +18,20 @@ public class Asteroids extends Rectangle{
 		this.color = color;
 	}
 
-	
-//	public void paint(Graphics g) {
-////		setSize(500,500);
-//		g.drawOval(100, 100, 50, 50, Color.black);
-//	}
-	
+		
 	public void draw(Graphics g){
 		
         Graphics2D g2d = (Graphics2D) g;
         // Translate to the centre of the spaceship
-        g2d.translate(this.x + this.width / 2, this.y + this.height / 2);
+//        g2d.translate(this.x + this.width / 2, this.y + this.height / 2);
         
         // Translate back to the original position
-        g2d.translate(-this.x - this.width / 2, -this.y - this.height / 2);
+//        g2d.translate(-this.x - this.width / 2, -this.y - this.height / 2);
 
 		g.setColor(this.color);
-        int[] xPoints = {this.x, this.x + this.width / 2, this.x + this.width};
-        int[] yPoints = {this.y, this.y + this.height, this.y};
-        int nPoints = 3;
+//        int[] xPoints = {this.x, this.x + this.width / 2, this.x + this.width};
+//        int[] yPoints = {this.y, this.y + this.height, this.y};
+//        int nPoints = 3;
 //        g.drawPolygon(xPoints, yPoints, nPoints);
         g.drawOval(100, 100, 50, 50);
 
